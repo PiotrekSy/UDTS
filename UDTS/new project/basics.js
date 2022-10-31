c6c07cacc8a61b99c8515f28034479d27e0d5fa7
PiotrekSy <piotrsyjud@gmail.com>
2022-10-31 13:26:12 +0100

    202210311326

--- UDTS/new project/basics.js
+++ UDTS/new project/basics.js
@@ -1,6 +1,14 @@
 var person = {
     name: "Piotrek",
-    age: 34
+    age: 34,
+    hobbies: ["Sports", "Film"],
+    role: [2, "admin"]
 };
 console.log(person.name);
 console.log(person.age);
+console.log(person.hobbies);
+console.log(person.hobbies[0]);
+console.log(person.hobbies[1]);
+console.log(person.role);
+console.log(person.role[0]);
+console.log(person.role[1]);
--- UDTS/new project/basics.ts
+++ UDTS/new project/basics.ts
@@ -1,13 +1,20 @@
 const person: {
   name: string;
   age: number;
-  role: [number, string];
+  hobbies: string[]; //tablica stringów
+  role: [number, string]; //Tuple
 } = {
   name: "Piotrek",
   age: 34,
+  hobbies: ["Sports", "Film"],
   role: [2, "admin"], // Tuple:
 };
 
-console.log(person.name);
-console.log(person.age);
-console.log(person.role);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.hobbies);
+// console.log(person.hobbies[0]);
+// console.log(person.hobbies[1]);
+// console.log(person.role);
+// console.log(person.role[0]);
+// console.log(person.role[1]);
