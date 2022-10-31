8bdfb85e3a867a99abd56a6ef03dcd93a9a7e736
PiotrekSy <piotrsyjud@gmail.com>
2022-10-31 13:44:37 +0100

    202210311344

--- UDTS/new project/basics.js
+++ UDTS/new project/basics.js
@@ -1,14 +1,43 @@
-var person = {
-    name: "Piotrek",
-    age: 34,
-    hobbies: ["Sports", "Film"],
-    role: [2, "admin"]
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[]; //tablica stringów
+//   role: [number, string]; //Tuple
+// } = {
+//   name: "Piotrek",
+//   age: 34,
+//   hobbies: ["Sports", "Film"],
+//   role: [2, "admin"], // Tuple:
+// };
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.hobbies);
+// console.log(person.hobbies[0]);
+// console.log(person.hobbies[1]);
+// console.log(person.role);
+// console.log(person.role[0]);
+// console.log(person.role[1]);
+// ENUMS:
+// enum Role {ADMIN = 6, READ_ONLY, AUTHOR};
+// const person = {
+//   name: "Piotrek",
+//   age: 34,
+//   hobbies: ["Sports", "Film"],
+//   role: Role[6] //enum used
+// };
+// console.log(person.role);
+// UNION TYPE
+var combine = function (input1, input2) {
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        var result = input1 + input2;
+        return result;
+    }
+    else {
+        var result = input1.toString() + input2.toString();
+        return result;
+    }
 };
-console.log(person.name);
-console.log(person.age);
-console.log(person.hobbies);
-console.log(person.hobbies[0]);
-console.log(person.hobbies[1]);
-console.log(person.role);
-console.log(person.role[0]);
-console.log(person.role[1]);
+var combinedAges = combine(30, 50);
+console.log(combinedAges);
+var combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
--- UDTS/new project/basics.ts
+++ UDTS/new project/basics.ts
@@ -1,14 +1,14 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[]; //tablica stringów
-  role: [number, string]; //Tuple
-} = {
-  name: "Piotrek",
-  age: 34,
-  hobbies: ["Sports", "Film"],
-  role: [2, "admin"], // Tuple:
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[]; //tablica stringów
+//   role: [number, string]; //Tuple
+// } = {
+//   name: "Piotrek",
+//   age: 34,
+//   hobbies: ["Sports", "Film"],
+//   role: [2, "admin"], // Tuple:
+// };
 
 // console.log(person.name);
 // console.log(person.age);
@@ -18,3 +18,34 @@ const person: {
 // console.log(person.role);
 // console.log(person.role[0]);
 // console.log(person.role[1]);
+
+// ENUMS:
+
+// enum Role {ADMIN = 6, READ_ONLY, AUTHOR};
+
+// const person = {
+//   name: "Piotrek",
+//   age: 34,
+//   hobbies: ["Sports", "Film"],
+//   role: Role[6] //enum used
+// };
+
+// console.log(person.role);
+
+// UNION TYPE
+
+const combine = (input1: number | string, input2: number | string) => {
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    const result = input1 + input2;
+    return result;
+  } else {
+    const result = input1.toString() + input2.toString();
+    return result;
+  }
+};
+
+const combinedAges = combine(30, 50);
+console.log(combinedAges);
+
+const combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
