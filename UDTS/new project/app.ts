44448b266f3bdaf04c05653512d58b53a6db891c
PiotrekSy <piotrsyjud@gmail.com>
2022-10-31 14:32:39 +0100

    202210311432

--- UDTS/new project/app.ts
+++ UDTS/new project/app.ts
@@ -1,27 +1,23 @@
-// const add = (a: number, b: number, showResult: boolean, phrase : string) =>{
-//     showResult ? console.log(a+b) : console.log(phrase + 'Odpowiedź z printresult = "false"')
-
-// }
-
-// const number1 = 5;
-// const number2 = 2.5;
-// const printResult = false;
-// const phrase = 'NIE WYŚWIETLAM ODPOWIEDZI! ';
-
-// add(number1, number2, printResult, phrase)
-
-const person = {
-  name: "Piotrek",
-  age: 34,
-  hobbies: ["Sport", "Film"],
-};
-
-let favouriteActivities: string[];
-favouriteActivities = ["Sports"];
-
-console.log(person.name);
-console.log(person.age);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase())
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
+
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
+
+function addAndHandle(n1: number, n2: number, callback: (num: number) => void) {
+  const result = n1 + n2;
+  callback(result);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+
+console.log(combineValues(8, 8));
+
+console.log(addAndHandle(19, 11, (result) => console.log(result)));
