87afdaf01dc6034f9acb43185727f931a76daf54
PiotrekSy <piotrsyjud@gmail.com>
2022-11-01 14:06:52 +0100

    repetitory

---
+++ UDTS/repetitory/src/app.ts
@@ -0,0 +1,12 @@
+// const userName = "Piotr";
+// let age = 30;
+// age = 18;
+
+// function add(a: number, b: number) {
+//   let result = a + b;
+//   return result;
+// }
+
+// console.log(add(2, 4));
+
+// age > 20 ? console.log("isOld") : console.log('isYoung');
