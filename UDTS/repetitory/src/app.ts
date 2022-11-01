4faabb8b33e94536ea91413e81b66331355be7b5
PiotrekSy <piotrsyjud@gmail.com>
2022-11-01 14:27:27 +0100

    repetitory2

--- UDTS/repetitory/src/app.ts
+++ UDTS/repetitory/src/app.ts
@@ -10,3 +10,35 @@
 // console.log(add(2, 4));
 
 // age > 20 ? console.log("isOld") : console.log('isYoung');
+
+// const button = document.querySelector("button")!;
+
+// button.addEventListener('click', (event) => console.log(event))
+
+// const add = (a: number, b: number = 1, c: number = 3) => a + b + c;
+
+// console.log(add(4, 9, 1));
+
+// console.log(add(4));
+
+const hobbies = ["Sports", "Cooking"];
+console.log([...hobbies]);
+console.log(...hobbies);
+
+const activeHobbies = ["Hiking"];
+console.log(activeHobbies);
+
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Piotr",
+  age: 34,
+};
+console.log(person);
+
+const copiedPerson = { ...person };
+console.log(copiedPerson);
